dc7950881d7d9097c26d23ad9a6c742e316469c9
jkc <1576523247@qq.com>
2022-01-06 17:46:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="container"></div>
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    ><a href="#" class="navbar-brand">者也专栏</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">登陆</a></li>
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item"></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "GlobalHeader",
+  components: {},
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
