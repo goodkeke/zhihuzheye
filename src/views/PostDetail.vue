fecd04091736bb803bb6e00709217ba5751e3f9a
jkc <1576523247@qq.com>
2022-01-21 09:07:33 +0800

    sb

---
+++ src/views/PostDetail.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="post-detail-page">
+    <article class="w-75 mx-auto mb-5 pb-3">
+      <img :src="currentImageUrl" class="rounded-lg img-fluid my-4" v-if="currentImageUrl"/>
+      <h2 class="mb-4">{{currentPost.title}}</h2>
+      <div class="user-profile-component border-top border-bottom py-3 mb-5 align-items-center row g-0">
+        <div class="col">
+          <user-profile :user="currentPost.author"
+          v-if="typeof currentPost.author === 'object'"></user-profile>
+        </div>
+        <span class="text-muted col text-right font-italic">
+          发表于：{{currentPost.createdAt}}
+        </span>
+      </div>
+      <div v-html="currentHTML"></div>
+    </article>
+  </div>
+</template>
+<script lang="ts">
+import {computed, defineComponent, onMounted} from 'vue'
+import { PostProps } from '../store'
+import { useStore } from "vuex";
+import { useRouter, useRoute } from "vue-router";
+import UserProfile from "../components/UserProfile.vue";
+import MarkdownIt from "markdown-it";
+export default defineComponent({
+  components: {
+    UserProfile
+  },
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const store = useStore()
+    console.log('route=======>', route)
+    console.log('router=======>', router)
+    const currentId = route.params.id
+    console.log('store==========>', currentId)
+    onMounted(() => {
+      store.dispatch('fetchPost', currentId)
+    })
+    const currentPost = computed<PostProps>(() => store.getters.getCurrentPost(currentId))
+
+    const md = new MarkdownIt()
+    const currentHTML = computed(() => {
+      const {content, isHTML} = currentPost.value
+      if(currentPost.value && content){
+        return isHTML ? content : md.render(content)
+      }
+    })
+    const currentImageUrl = computed(() => {
+      if(currentPost.value && currentPost.value.image){
+        const {image} = currentPost.value
+        return (image as ImageProps).url + '?x-oss-process=image/resize,w_850'
+      }else {
+        return null
+      }
+    })
+    return {
+      currentId,
+      currentPost,
+      currentHTML,
+      currentImageUrl
+    }
+  }
+})
+</script>
